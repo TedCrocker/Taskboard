dd56d712feb56c47104d819a80945920ba7f6809
ted.crocker <ted.crocker@trapwire.com>
2013-09-23 15:36:01 -0400

    Starting to add in the data layer.

---
+++ Taskboard/DataAccess/IDataSource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taskboard.DataAccess
+{
+	public interface IDataSource
+	{
+		IEntity Add(int id, IEntity entity);
+		IEntity Update(int id, IEntity entity);
+		IEntity Fetch(int id);
+		void Delete(int id);
+	}
+}
