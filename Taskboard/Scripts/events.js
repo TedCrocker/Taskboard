127e763c14710883871e774a8e81fe0432ae5484
ted.crocker <ted.crocker@trapwire.com>
2013-09-30 09:55:02 -0400

    Added event handler file. Moved hub stuff into its own file.

---
+++ Taskboard/Scripts/events.js
@@ -0,0 +1,37 @@
+﻿(function (events)
+{
+	events.connection = {};
+	events.connection.started = "events.connection.started";
+
+	var _eventMappings = {};
+
+	function initEvent(eventName)
+	{
+		var event = document.createEvent("Event");
+		event.initEvent(eventName, true, true);
+		_eventMappings[eventName] = event;
+	}
+
+	events.publish = function (eventName)
+	{
+		if (_eventMappings[eventName])
+		{
+			document.dispatchEvent(_eventMappings[eventName]);
+		}
+		else
+		{
+			console.error("The event " + eventName + " does not currently exist");
+		}
+	};
+
+	events.subscribe = function (eventName, handler)
+	{
+		if (!_eventMappings[eventName])
+		{
+			initEvent(eventName);
+		}
+
+		document.addEventListener(eventName, handler, false);
+	};
+
+})(window.events = window.events || {});
---
+++ Taskboard/Scripts/hubs.js
@@ -0,0 +1,59 @@
+﻿(function(hubs, events, $)
+{
+	var chatHub = $.connection.chatHub;
+	var taskHub = $.connection.taskHub;
+
+	function setupChatHub()
+	{
+		$('#displayname').val(prompt("Enter your name:", ""));
+		$('#message').focus();
+
+
+		chatHub.client.addNewMessageToPage = function (name, message)
+		{
+			$('#discussion').append("<li><strong>" + htmlEncode(name) + "</strong>: " + htmlEncode(message) + "</li>");
+		};
+
+		events.subscribe(events.connection.started, function (e)
+		{
+			$('#sendmessage').click(function ()
+			{
+				var displayName = $('#displayname').val();
+				var message = $('#message').val();
+
+				chatHub.server.send(displayName, message);
+				$('#message').val('').focus();
+			});
+		});
+	}
+
+	function setupTaskHub()
+	{
+		var tasks = [];
+		taskHub.client.addTask = function(model)
+		{
+			tasks.push(model);
+			$('<div/>').text(model.content)
+					.css({ left: model.left, top: model.top, "background-color": "red" })
+					.addClass('task').appendTo($('body'));
+		};
+
+		events.subscribe(events.connection.started, function (e)
+		{
+
+		});
+	}
+
+	function htmlEncode(value)
+	{
+		return $('<div/>').text(value).html();
+	}
+
+	$.connection.hub.start().done(function ()
+	{
+		events.publish(events.connection.started);
+	});
+
+	setupChatHub();
+	setupTaskHub();
+})(window.hubs = window.hubs || {}, window.events = window.events || {}, jQuery);
