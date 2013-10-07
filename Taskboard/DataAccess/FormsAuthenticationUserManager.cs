f6012dc0e59be3acc6c58eac4bae1c1c642c9915
ted.crocker <ted.crocker@trapwire.com>
2013-10-07 12:37:17 -0400

    Adding in some stuff for eventualy authentication stuff.

---
+++ Taskboard/DataAccess/FormsAuthenticationUserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Security;
+
+namespace Taskboard.DataAccess
+{
+	public class DummyFormsAuthenticationUserManager : IUserManager
+	{
+
+		public bool Authenticate(string userName, string password)
+		{
+			if (password == "fail")
+			{
+				return false;
+			}
+
+			FormsAuthentication.SetAuthCookie(userName, true);
+
+			return true;
+		}
+
+		public void Unauthenticate()
+		{
+			FormsAuthentication.SignOut();
+		}
+	}
+}
---
+++ Taskboard/DataAccess/IUserManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Taskboard.DataAccess
+{
+	public interface IUserManager
+	{
+		bool Authenticate(string userName, string password);
+		void Unauthenticate();
+	}
+}
