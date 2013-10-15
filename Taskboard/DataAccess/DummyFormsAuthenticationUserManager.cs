ca4f62b570173ff5cd1b58590e331784bb5329c5
Ted.Crocker <edward.b.crocker@gmail.com>
2013-10-15 14:43:23 -0400

    Fixed authentication bug

--- Taskboard/DataAccess/DummyFormsAuthenticationUserManager.cs
+++ Taskboard/DataAccess/DummyFormsAuthenticationUserManager.cs
@@ -1,27 +1,41 @@
 ﻿using System;
+using System.Net;
+using System.Web;
 using System.Web.Security;
 
 namespace Taskboard.DataAccess
 {
 	public class DummyFormsAuthenticationUserManager : IUserManager
 	{
-		private static string _displayName;
-
 		public string DisplayName
 		{
-			get { return _displayName; }
+			get
+			{
+				var httpCookie = HttpContext.Current.Request.Cookies["UserName"];
+				if (httpCookie != null)
+				{
+					return httpCookie.Value;
+				}
+				return "";
+			}
 		}
 
 		public bool Authenticate(string userName, string password)
 		{
 			if (password != "Password123TrapWire")
 			{
-				_displayName = null;
+				if (HttpContext.Current.Request.Cookies["UserName"] != null)
+				{
+					HttpContext.Current.Response.Cookies.Remove("UserName");
+				}
 				return false;
 			}
 
 			FormsAuthentication.SetAuthCookie(userName, true);
-			_displayName = userName;
+
+			var cookie = new HttpCookie("UserName");
+			cookie.Value = userName;
+			HttpContext.Current.Response.Cookies.Add(cookie);
 
 			return true;
 		}
