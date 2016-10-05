1da0adc0fda3fb5b23c582785e5e2e70de800c12
Ted Crocker <edward.b.crocker@gmail.com>
2016-10-05 15:48:12 -0400

    Loaded system.js stuff correctly.

--- Taskboard/Scripts/new/main.js
+++ Taskboard/Scripts/new/main.js
@@ -1,4 +1,4 @@
-System.import([], function(exports_1, context_1) {
+System.register([], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     return {
